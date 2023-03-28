f463c681730691436c240ea3bc74619f2dd5e40e
ssumkin <rms856@gmail.com>
2023-03-28 17:40:40 +0900

    js update

--- index.js
+++ index.js
@@ -1,6 +1,46 @@
+for(let i = 0; i < 5; i++) {
+    for(let j = 0; j <= i; j++) {
+        document.write("*");
+    }
+    document.write("<br>");
+}
+
+document.write("<br>");
+
 for(let i = 5; i > 0; i--) {
     for(let j = i; j > 0; j--) {
         document.write("*");
     }
     document.write("<br>");
 }
+
+document.write("<br>");
+
+for(let i = 0; i < 5; i++) {
+    for(let j = 4; j > i; j--) {
+        document.write("&nbsp;");
+    } 
+    for(let j = 0; j <= i; j++) {
+        document.write("*");
+    }
+
+    document.write("<br>");
+}
+
+ 
+ 
+
+for(let i = 0; i < 5; i++) {
+    let gap = "";
+    let star = "";
+
+    for(let j = 4; j > i; j--) {
+        gap += " ";
+    } 
+    for(let j = 0; j <= i; j++) {
+        star += "*";
+    }
+    console.log(gap + star); 
+
+}
+ 
